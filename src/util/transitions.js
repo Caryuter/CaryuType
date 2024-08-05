2f5a67beba0c04ec7544c69b16dd943790334cf5
Caryuter <caryuter162006@gmail.com>
2024-08-05 13:17:49 -0600

    Feat: Add CSS support to transitions lib

--- src/util/transitions.js
+++ src/util/transitions.js
@@ -1,30 +1,35 @@
 export {beginTransition}
-import { arraysAreIdentical, isInDOM, removeFirstOccurrence } from "./utils.js"
+import { arraysAreIdentical, cloneObject, isInDOM, removeFirstOccurrence } from "./utils.js"
 
 
 
 /**
- * Handles different types of transitions based on the provided options.
+ * Handles different types of transitions based on the provided options. 
+ * By default if no neecesary option is provided it searches in computed styles;
+ * it can be disabled with `disableCSS` option
+ * 
+ * If no `options.duration` or ``timingFunction`` is provided either on `options` or  computed styles,
+ * it defaults to **1s** and  **ease** respectively
  * @param {HTMLElement} el - The element to be animated
  * @param {"collapse"|"expand"|"move"|"hide-and-do"|"opacity"} type - Type of transition 
  * @param {Object} options - Options for the transition
- * @param {Number} [options.duration] - Duration of the transition in seconds
+ * @param {String} [options.duration] - Duration of the transition **must declare units**
  * @param {"ease"|"ease-in"|"ease-out"|"ease-in-out"|"linear"} [options.timingFunction] - Timing function for the transition
- * @param {Number} [options.delay] - Delay before the transition starts in seconds
+ * @param {String} [options.delay] - Delay before the transition starts **must declare units**
  * @param {Number} [options.xPos] - x position for move transition
  * @param {Number} [options.yPos] - y position for move transition
  * @param {Number} [options.desiredWidth] - Desired width for collapse or expand transition in pixels
  * @param {Number} [options.desiredOpacity] - Desired opacity for opacity transitions in range between 0 and 1
  * @param {Function} [options.callback] - Callback function for hide transition
+ * @param {Boolean} disableCSS - if checked disables default computed styles, defaults to *false*
  * @returns {Promise} A promise that resolves when the transition ends
  */
-function beginTransition(el,type, options) {
+function beginTransition(el,type, options, disableCSS = false) {
 
   if(!isInDOM(el)){
     return Promise.reject(new Error(`Passed value for transition ${this} isn't an element`))
   }
-  if(!options.duration) options.duration = 1
-  if(!options.timingFunction) options.timingFunction = "ease"
+  options.disableCSS = disableCSS
   switch (type) {
     case 'collapse':
       return collapseElement(el, options);
@@ -57,10 +62,11 @@ function collapseElement(el, options){
   let prevWidth = Math.round(el.getBoundingClientRect().width)
   if(prevWidth == desiredWidth) return Promise.resolve()
 
-  addTransition(el, "width", options)
+  
   
   return transitOpacity(el, options)
   .then(() =>{
+    addTransition(el, "width", options)
     el.style.width = prevWidth + "px"
     el.offsetWidth // force repaint
     el.style.width = desiredWidth + 'px'
@@ -74,6 +80,7 @@ function collapseElement(el, options){
   })
 }
 
+//TODO: Doesn't expands element
 /**
  * Expands an element to its auto width
  * @param {HTMLElement} el The element to expand
@@ -81,7 +88,6 @@ function collapseElement(el, options){
  * @return {Promise} promise resolving when transition ends
  */
 function expandElement(el, options){
-  options.duration == options.duration /2
   if(!options.desiredOpacity) options.desiredOpacity = 1
   el.offsetWidth // force repaint
 
@@ -116,10 +122,9 @@ function moveAbsoluteElement(el, options){
   let prevY = Math.round(top) + Math.round(height)/2
   if(prevY == yPos && prevX == xPos) return Promise.resolve()
 
+  addTransition(el, ["top", "left"], options)
   el.style.top = yPos + "px"
   el.style.left = xPos + "px"
-
-  addTransition(el, ["top", "left"], options)
   return waitForTransitions(el, ["top", "left"])
 }
 
@@ -131,8 +136,8 @@ function moveAbsoluteElement(el, options){
  */
 function hideAndDo(el, options){
   if(!options.callback) return Promise.reject(new Error("No callback passed por element: " + el.classList))
-    
   options.desiredOpacity = 0
+
   return transitOpacity(el, options)
   .then(() => Promise.resolve(options.callback()))
   .then(() => {
@@ -148,11 +153,11 @@ function hideAndDo(el, options){
  * @return {Promise} a promise that resolves when transition ends
  */
 function transitOpacity(el, options = {}){
-
   let {desiredOpacity} = options
   if (isNaN(desiredOpacity)) return Promise.reject(new Error("Didn't passed desired opacity for transitioning element"))
   let prevOpacity = getComputedStyle(el).getPropertyValue("opacity")
   if(prevOpacity == desiredOpacity) return Promise.resolve()
+
   addTransition(el, "opacity", options)
   el.style.opacity = desiredOpacity
   return waitForTransitions(el, "opacity")
@@ -182,13 +187,14 @@ function waitForTransitions(el, properties){
   })
 }
 
+//TODO: Doesn't work properly
 /**
  * Removes only the transition property from the specified element 
  * @param {HTMLElement} el - element to be removed the property from 
  * @param {String} property - css property to remove 
  */
 function removeTransitionProperty(el, property){
-  let transitions = el.style.transition.split(",").map((transition) => transition.trim())
+  let transitions =  el.style.transition.split(",").map((transition) => transition.trim())
   let updatedTransitions = removeFirstOccurrence(transitions,property)
   if(arraysAreIdentical(transitions, updatedTransitions)) {
     console.warn(`can't remove ${property} transition on ${el.tagName}: ${el.classList.item(0)}`)
@@ -202,27 +208,27 @@ function removeTransitionProperty(el, property){
  * @param {HTMLElement} el - The element to which the transition will be added
  * @param {String|Array<String>} properties - The CSS property or properties to transition
  * @param {Object} [options] - Optional transition parameters
- * @param {Number} [options.duration] - The duration of the transition in seconds
- * @param {"ease"|"ease-in"|"ease-out"|"ease-in-out"|"linear"} [options.timingFunction] - The timing function for the transition (e.g., 'linear', 'ease')
- * @param {Number} [options.delay] - The delay before the transition starts in seconds
  */
 function addTransition(el, properties, options = {}) {
   if(typeof properties == "string"){
     properties = [properties]
   }
 
+
   let newTransitions = properties.map((property) => {
+    let propertyOptions = applyDefaultOptions(el, property, options)
+    console.log(property,propertyOptions);
     let newTransition = property;
-    if (options.duration !== undefined && options.duration !== '') {
-      newTransition += ` ${options.duration}s`;
+    if (propertyOptions.duration !== undefined && propertyOptions.duration !== '') {
+      newTransition += ` ${propertyOptions.duration}`;
     }
 
-    if (options.timingFunction !== undefined && options.timingFunction !== '') {
-      newTransition += ` ${options.timingFunction}`;
+    if (propertyOptions.timingFunction !== undefined && propertyOptions.timingFunction !== '') {
+      newTransition += ` ${propertyOptions.timingFunction}`;
     }
 
-    if (options.delay !== undefined && options.delay !== '') {
-      newTransition += ` ${options.delay}s`;
+    if (propertyOptions.delay !== undefined && propertyOptions.delay !== '') {
+      newTransition += ` ${propertyOptions.delay}`;
     }
     return newTransition
   })
@@ -234,6 +240,7 @@ function addTransition(el, properties, options = {}) {
   } else {
     el.style.transition = newTransitions.join(", ");
   }
+  console.log(el.style.transition);
 }
 
 /**
@@ -252,4 +259,43 @@ function getAutoDimensions(el){
       const autoHeight = clone.offsetHeight;
       document.body.removeChild(clone);
       return { width: autoWidth, height: autoHeight };
+}
+
+/**
+ * Applies default transition styles by searching on conputed styles
+ * @param {HTMLElement} el - element to check styles on
+ * @param {String} porperty - property to check styles on
+ * @param {Object} options - Base options 
+ */
+function applyDefaultOptions(el, property, options){
+  let optionsCopy = cloneObject(options)
+  let prop = getComputedStyle(el).transitionProperty.split(",").map(s => s.trim())
+  if(!options.disableCSS){
+    let index = prop.findIndex(s => s == property || s == "all")
+    console.log(prop,index,property);
+    if(index == -1){
+      console.warn("can't apply default styles to a property that doesn't exists on element")
+      return optionsCopy
+    }
+    // get calculated css transition properties for element
+    let {transitionProperty, transitionDuration, transitionTimingFunction, transitionDelay} = getComputedStyle(el)
+    //group transitions properties in array to traverse it
+    let transitionproperties = [transitionProperty, transitionDuration, transitionTimingFunction, transitionDelay];
+    //get property for transition type index and re-destructure it
+    [transitionProperty, transitionDuration, transitionTimingFunction, transitionDelay] = transitionproperties.map(
+      (val) => {
+        return val.split(",").map(s => s.trim())[index]
+    })
+    console.log(transitionTimingFunction);
+    const numberRegex = /[\d.]+/
+
+    //Check if value isn't configured and if number is greater than 0
+    if(!optionsCopy.duration && transitionDuration.match(numberRegex)[0] > 0) optionsCopy.duration = transitionDuration
+    if(!optionsCopy.timingFunction) optionsCopy.timingFunction = transitionTimingFunction
+    if(!optionsCopy.delay && transitionDelay.match(numberRegex)[0] > 0) optionsCopy.delay = transitionDelay
+  }
+  if(!optionsCopy.timingFunction) optionsCopy.timingFunction = "ease"
+  if(!optionsCopy.duration) optionsCopy.duration = "0.4s"
+
+  return optionsCopy
 }
